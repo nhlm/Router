3cfa6aa90ca09255a532233e23e11331adf33a6a
Payam Naderi <naderi.payam@gmail.com>
2015-08-23 16:07:07 +0430

    documentation comment

--- Http/HAbstractChainRouter.php
+++ Http/HAbstractChainRouter.php
@@ -10,6 +10,39 @@ use Poirot\PathUri\HttpUri;
 use Poirot\Router\Interfaces\Http\iHChainingRouter;
 use Poirot\Router\Interfaces\Http\iHRouter;
 
+/**
+ * [code]
+ *   $Router = new RChainStack('main');
+ *   $Router
+ *       ->add(new RSegment('pages',
+ *               [
+ *                   'criteria'    => '/pages',
+ *                   'exact_match' => false,
+ *               ]
+ *               , ['__action__' => 'check_user']
+ *           )
+ *       )->recent() ## recently added segment route "pages" as chain router
+ *       ->link(new RSegment('static',
+ *           [
+ *               'criteria'    => 'static/mypage',
+ *           ]
+ *           , ['__action__' => 'display_static_mypage']
+ *       ))
+ *       ->add(new RSegment('page', ## add to "pages" recently router
+ *           [
+ *               'criteria'    => [':request_page' => ['request_page'=>'\w+'], ],
+ *           ]
+ *           , ['__action__' => 'display_page']
+ *       ))
+ *       ->parent() ## get back to parent "main" chain router stack
+ *       ->add(new RSegment('auth',
+ *           [
+ *               'criteria'    => '/register',
+ *           ]
+ *       ));
+ *   ;
+ * [/code]
+ */
 class HAbstractChainRouter extends HAbstractRouter
     implements iHChainingRouter
 {
