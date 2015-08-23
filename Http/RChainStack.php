74a5976cbd42741d6fcbc41c321ea06dcf841b95
Payam Naderi <naderi.payam@gmail.com>
2015-08-23 16:07:39 +0430

    init stack router improvement.

--- Http/RChainStack.php
+++ Http/RChainStack.php
@@ -1,40 +1,93 @@
 <?php
 namespace Poirot\Router\Http;
 
-/**
- * [code]
-    $Router = new RChainStack('main');
-    $Router
-        ->add(new RSegment('pages',
-                [
-                    'criteria'    => '/pages',
-                    'exact_match' => false,
-                ]
-                , ['__action__' => 'check_user']
-            )
-        )->recent() ## recently added segment route "pages" as chain router
-        ->link(new RSegment('static',
-            [
-                'criteria'    => 'static/mypage',
-            ]
-            , ['__action__' => 'display_static_mypage']
-        ))
-        ->add(new RSegment('page', ## add to "pages" recently router
-            [
-                'criteria'    => [':request_page' => ['request_page'=>'\w+'], ],
-            ]
-            , ['__action__' => 'display_page']
-        ))
-        ->parent() ## get back to parent "main" chain router stack
-        ->add(new RSegment('auth',
-            [
-                'criteria'    => '/register',
-            ]
-        ));
-    ;
- * [/code]
- */
+use Poirot\Router\Interfaces\Http\iHRouter;
+
 class RChainStack extends HAbstractChainRouter
 {
+    /**
+     * @inheritdoc
+     *
+     * @param array $factArr Builder Factory Config
+     *
+     * @throws \InvalidArgumentException
+     * @return iHRouter
+     */
+    static function factory(array $factArr)
+    {
+        /** @var RChainStack $instance */
+        $instance = parent::factory($factArr);
 
+        if (isset($factArr['routes']))
+            $instance->addRoutes($factArr['routes']);
+
+        return $instance;
+    }
+
+    /**
+     * Add routes
+     *
+     * ! routes: [
+     *      // RSegment::factory([ ...
+     *      ## or
+     *      'pages' => [ ## route name
+     *         'route' => 'segment', ## route instance as service name
+     *          ## ...
+     *         'options' => [],
+     *         'params'  => [],
+     *          ## add child routes
+     *         'routes' => [
+     *              RSegment::factory([
+     *                   'name' => 'page', ## route name "pages/page"
+     *                    ## ...
+     *                   'options' => [],
+     *                   'params'  => [],
+     *              ]),
+     *          ],
+     *     ], // end pages route
+     *   ]
+     *
+     * @param array $routes
+     *
+     * @throws \InvalidArgumentException
+     * @return $this
+     */
+    function addRoutes(array $routes)
+    {
+        foreach($routes as $rn => $ro) {
+            if (is_string($rn) && is_array($ro))
+                $this->__addAssocRoute($rn, $ro);
+            elseif (is_int($rn) && $ro instanceof iHRouter)
+                $this->__addInstanceRoute($ro);
+            else
+                throw new \InvalidArgumentException(sprintf(
+                    'Invalid argument provided. ("%s")'
+                    , serialize($ro)
+                ));
+        }
+
+        return $this;
+    }
+
+    /**
+     * - if link leaf is empty add route by link
+     *
+     * : 'pages' => [ ## route name
+     *         'route' => 'segment', ## route instance as service name
+     *          ## ...
+     *         'options' => [],
+     *   ...
+     *
+     * @param string $routeName
+     * @param array  $options
+     */
+    protected function __addAssocRoute($routeName, array $options)
+    {
+
+    }
+
+    protected function __addInstanceRoute(iHRouter $route)
+    {
+
+    }
 }
