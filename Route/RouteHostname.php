dbe6ae55d70be094f65824baaa5edf7a9af95426
Payam Naderi <naderi.payam@gmail.com>
2016-05-23 13:20:31 +0430

    cleanup tokenizer

--- Route/RouteHostname.php
+++ Route/RouteHostname.php
@@ -40,12 +40,36 @@ class RouteHostname
      * !! don`t change request object attributes
      *
      * @param RequestInterface $request
-     *
-     * @return iRoute|false
+     * @return false|iRoute
+     * @throws \Exception
      */
     function match(RequestInterface $request)
     {
-        $routerMatch = $this->_match($request, $this->getCriteria());
+        ## host may have port
+        /** @var RequestInterface $request */
+        $host = $request->getHeaderLine('Host');
+        if (!$host)
+            throw new \Exception('Host not recognized in Request.');
+
+
+        $parts      = \Poirot\Std\Lexer\parseDefinition($this->getCriteria());
+        $regexMatch = \Poirot\Std\Lexer\buildRegexFromParsed($parts);
+        $result     = preg_match('(^' . $regexMatch . '$)', $host, $matches);
+
+        if (!$result)
+            ## route not matched
+            return false;
+
+        
+        $params = array();
+        foreach ($matches as $index => $val) {
+            if (is_int($index)) continue;
+
+            $params[$index] = $val;
+        }
+
+        $routerMatch = clone $this;
+        $routerMatch->params()->import($params);
         return $routerMatch;
     }
 
@@ -173,157 +197,5 @@ class RouteHostname
             return '';
 
         return $host;
-    }
-    
-
-    protected function _match(RequestInterface $request, $criteria)
-    {
-        ## host may have port
-        /** @var RequestInterface $request */
-        $host = $request->getHeaderLine('Host');
-        if (!$host)
-            throw new \Exception('Host not recognized in Request.');
-
-
-        $parts      = $this->_parseStringDefinition($criteria);
-
-        kd($parts);
-
-        $buildRegex = $this->_buildRegex($parts, array());
-        $result     = preg_match('(^' . $buildRegex . '$)', $host, $matches);
-
-        if (!$result)
-            ## route not matched
-            return false;
-
-        $params = array();
-        foreach ($this->_paramMap as $index => $name) {
-            if (isset($matches[$index]) && $matches[$index] !== '')
-                $params[$name] = $matches[$index];
-        }
-
-        $routerMatch = clone $this;
-        $routerMatch->params()->import($params);
-        return $routerMatch;
-    }
-
-    /**
-     * Parse a string variable/literal definition.
-     * 
-     * @param  string $interchange
-     * @return array
-     * @throws \RuntimeException
-     */
-    protected function _parseStringDefinition($interchange)
-    {
-        $currentPos = 0;
-        $length     = strlen($interchange);
-
-        $parts      = array();
-        $levelParts = array(&$parts);
-        $level      = 0;
-
-        while ($currentPos < $length)
-        {
-            ## the tokens are .:{[]
-            preg_match('(\G(?P<_literal_>[A-Za-z0-9-]*)(?P<_token_>[.:{\[\]]|$))'
-                , $interchange
-                , $matches
-                , 0
-                , $currentPos
-            );
-            $currentPos += strlen($matches[0]);
-
-            if (!empty($matches['_literal_']))
-                $levelParts[$level][] = array('_literal_' => $matches['_literal_']);
-
-            # Deal With Token:
-            if (!isset($matches['_token_']))
-                continue;
-
-            $Token = $matches['_token_'];
-            if ($Token === ':') {
-                $pmatch = preg_match('(\G(?P<_name_>[^:.{\[\]]+)(?:{(?P<_delimiter_>[^}]+)})?:?)'
-                    , $interchange
-                    , $matches
-                    , 0
-                    , $currentPos
-                );
-                if (!$pmatch)
-                    throw new \RuntimeException('Found empty parameter name');
-
-                $parameter = $matches['_name_'];
-                $val       = array('_parameter_' => $parameter);
-                if (isset($matches['_delimiter_']))
-                    $val[$parameter] = $matches['_delimiter_'];
-
-                $levelParts[$level][] = $val;
-                $currentPos += strlen($matches[0]);
-            }
-
-            if ($Token === '[') {
-                $va = array();
-                $levelParts[$level][]   = array('_optional_' => &$va);
-
-                $level++;
-                $levelParts[$level] = &$va;
-            }
-            
-            if ($Token === ']') {
-                unset($levelParts[$level]);
-                $level--;
-
-                if ($level < 0)
-                    throw new \RuntimeException('Found closing bracket without matching opening bracket');
-            }
-            
-        } // end while
-
-        if ($level > 0)
-            throw new \RuntimeException('Found unbalanced brackets');
-
-        return $parts;
-    }
-
-    /**
-     * Build the matching regex from parsed parts.
-     *
-     * @param  array   $parts
-     * @param  array   $constraints
-     * @param  int $groupIndex
-     * @return string
-     * @throws \RuntimeException
-     */
-    protected function _buildRegex(array $parts, array $constraints, &$groupIndex = 1)
-    {
-        $regex = '';
-
-        foreach ($parts as $part) {
-            switch ($part[0]) {
-                case 'literal':
-                    $regex .= preg_quote($part[1]);
-                    break;
-
-                case 'parameter':
-                    $groupName = '?P<param' . $groupIndex . '>';
-
-                    if (isset($constraints[$part[1]])) {
-                        $regex .= '(' . $groupName . $constraints[$part[1]] . ')';
-                    } elseif ($part[2] === null) {
-                        $regex .= '(' . $groupName . '[^.]+)';
-                    } else {
-                        $regex .= '(' . $groupName . '[^' . $part[2] . ']+)';
-                    }
-
-                    $this->_paramMap['param' . $groupIndex++] = $part[1];
-                    break;
-
-                case 'optional':
-                    $regex .= '(?:' . $this->_buildRegex($part[1], $constraints, $groupIndex) . ')?';
-                    break;
-            }
-        }
-
-        return $regex;
     }
 }
